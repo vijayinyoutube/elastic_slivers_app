3103cd5fe665deaaaa2f4c0d6b7155465d60a52c
vijayinyoutube <vijaythevirtue@gmail.com>
2022-11-26 15:51:59 +0530

    initial setup

---
+++ lib/Presentation/Routes/generated_routes.dart
@@ -0,0 +1,28 @@
+import 'package:elastic_slivers_app/Presentation/Screens/Homepage/UI/home_page.dart';
+import 'package:flutter/material.dart';
+
+class RouteGenerator {
+  Route<dynamic> generateRoute(RouteSettings settings) {
+    final args = settings.arguments;
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(
+            builder: (_) => const HomePage(title: 'Elastic Sliver'));
+      default:
+        return _errorRoute();
+    }
+  }
+
+  static Route<dynamic> _errorRoute() {
+    return MaterialPageRoute(builder: (_) {
+      return Scaffold(
+        appBar: AppBar(
+          title: const Text('Error'),
+        ),
+        body: const Center(
+          child: Text('ERROR'),
+        ),
+      );
+    });
+  }
+}
