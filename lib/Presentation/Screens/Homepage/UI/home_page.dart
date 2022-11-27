4948be66498a17e9985bde2b9010749b6011eef7
vijayinyoutube <vijaythevirtue@gmail.com>
2022-11-27 21:31:40 +0530

    refactor

--- lib/Presentation/Screens/Homepage/UI/home_page.dart
+++ lib/Presentation/Screens/Homepage/UI/home_page.dart
@@ -1,4 +1,8 @@
+import 'package:elastic_slivers_app/Presentation/Components/spacer.dart';
+import 'package:elastic_slivers_app/Presentation/Declarations/constants.dart';
 import 'package:flutter/material.dart';
+
+import '../Widgets/search_bar.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key, required this.title});
@@ -7,6 +11,72 @@ class HomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          const SliverAppBarBldr(),
+          SliverToBoxAdapter(child: HeightSpacer(myHeight: kSpacing)),
+          const SliverSearch(),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (BuildContext context, int index) {
+                return Padding(
+                  padding: kPadding/2,
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: kBorderRadius,
+                      color: secondaryColor.withOpacity(0.3),
+                    ),
+                    height: 200,
+                    width: MediaQuery.of(context).size.width,
+                  ),
+                );
+              },
+              childCount: 20,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class SliverSearch extends StatelessWidget {
+  const SliverSearch({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return  SliverAppBar(
+        backgroundColor:  Theme.of(context).scaffoldBackgroundColor,
+        elevation: 0,
+        pinned: true,
+        flexibleSpace: const Center(child: SearchBar()));
+  }
+}
+
+class SliverAppBarBldr extends StatelessWidget {
+  const SliverAppBarBldr({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const SliverAppBar(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      pinned: true,
+      centerTitle: false,
+      stretch: true,
+      expandedHeight: 300.0,
+      flexibleSpace: FlexibleSpaceBar(
+        stretchModes: [StretchMode.zoomBackground],
+        background: Image(
+          image: AssetImage('assets/Images/feature.png'),
+          fit: BoxFit.cover,
+        ),
+      ),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Elastic Sliver',
       theme: ThemeData(
         primarySwatch: Colors.blue,
